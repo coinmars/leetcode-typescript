e5e9960b4b236fee23398fca49fc074e251e1206
Stark <43443357+stark-eth@users.noreply.github.com>
2023-03-24 12:38:24 +0800

    Update 709-toLowerCase.ts

--- 709-toLowerCase.ts
+++ 709-toLowerCase.ts
@@ -12,8 +12,3 @@ function toLowerCase(str: string): string {
   const result = lowerArr.join("");
   return result;
 }
-
-作者：james
-链接：https://leetcode.cn/problems/to-lower-case/solutions/284586/javascripttypescript-li-yong-asciima-jin-xing-zhua/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
